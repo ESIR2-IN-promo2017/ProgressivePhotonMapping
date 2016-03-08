f739b4bd481f8c6990e2cd640d252e6214ac58e1
TheMrNomis <n0m1s@homnomnom.fr>
2016-03-08 09:45:33 +0100

    matlab code for SSIM

---
+++ param_tests/code/SSIM.m
@@ -0,0 +1,15 @@
+close all;
+clear;
+clc;
+
+refImg = imread("../data/cbox.png");
+
+nbImg = 69;
+
+ssims = zeros(nbImg,1)
+
+for i = 1:nbImg
+    i
+    testImg = imread(["../data/" num2str(i) ".png"]);
+    ssims(i) = ssim(testImg, nbImg);
+end
