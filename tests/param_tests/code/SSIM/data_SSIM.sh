537f7da10b9d06d9bbf1053c1fe687339f760af9
TheMrNomis <n0m1s@homnomnom.fr>
2016-03-15 22:50:22 +0100

    directory structure

--- tests/param_tests/code/SSIM/data_SSIM.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "SSIM R,SSIM G,SSIM B" > SSIM.results
-
-for IMG in $(ls ../../data/ | grep -v cbox.png)
-do
-    echo $IMG
-    ./ssim_test.out ../../data/cbox.png ../../data/$IMG >> SSIM.results
-done
